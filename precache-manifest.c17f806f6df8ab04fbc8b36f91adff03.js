70775580bb67454a91fb3b17c495c8643fdbdb75
Mirel Home <mirelalex2094@gmail.com>
2020-04-06 13:20:04 +0300

    Updates

---
+++ precache-manifest.c17f806f6df8ab04fbc8b36f91adff03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ca3da2d2f6cb09fd636d6d4a1047d6",
+    "url": "/Ochanissi/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "5bd8b403d0fa782389c3",
+    "url": "/Ochanissi/Monsters-Rolodex/static/css/main.130922cd.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/Ochanissi/Monsters-Rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ochanissi/Monsters-Rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd8b403d0fa782389c3",
+    "url": "/Ochanissi/Monsters-Rolodex/static/js/main.8c15e83d.chunk.js"
+  },
+  {
+    "revision": "631331ca0eac5ddde366",
+    "url": "/Ochanissi/Monsters-Rolodex/static/js/runtime-main.793c15ad.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ochanissi/Monsters-Rolodex.git/precache-manifest.02d71cbe7ff5c267825a50d3bd8e254c.js"
+  "/Ochanissi/Monsters-Rolodex/precache-manifest.c17f806f6df8ab04fbc8b36f91adff03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ochanissi/Monsters-Rolodex.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ochanissi/Monsters-Rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
