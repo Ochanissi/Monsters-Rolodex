7e9b5481ce16603174d7f6c68281da5a177feaae
Mirel Home <mirelalex2094@gmail.com>
2020-04-06 10:24:08 +0300

    creating the search-box component and styled it

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ import './card-list.styles.css';
 
 export const CardList = (props) => {
   return (
-    <div className="card-list">
+    <div className='card-list'>
       {props.monsters.map((monster) => (
         <Card key={monster.id} monster={monster} />
       ))}
