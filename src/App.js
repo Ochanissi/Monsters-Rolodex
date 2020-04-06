08af4a3cfa45b01d43662befcbf020d9f81e2988
Mirel Home <mirelalex2094@gmail.com>
2020-04-06 13:06:57 +0300

    added the styles and header

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends Component {
     );
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -27,9 +31,10 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='Search monsters'
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
